c0ef476fed2bd6162543bbb38df0f46ac983249d
alicanov98 <alicanov1998@gmail.com>
2023-02-21 05:39:11 +0400

    add to the payment page

--- assets/js/main.js
+++ assets/js/main.js
@@ -89,55 +89,76 @@ function openDropdown() {
   }
 }
 
-let sum=document.querySelector("#qty").value;
 
-function increment(){
-sum++
+
+function increment() {
+  document.querySelector("#qty").value++;
 }
 
-function decrement(){
-sum--
+function decrement() {
+  document.querySelector("#qty").value--;
 }
 
 const labels = document.querySelectorAll(".accordion-item__label");
 const tabs = document.querySelectorAll(".accordion-tab");
 
 function toggleShow() {
-	const target = this;
-	const item = target.classList.contains("accordion-tab")
-		? target
-		: target.parentElement;
-	const group = item.dataset.actabGroup;
-	const id = item.dataset.actabId;
+  const target = this;
+  const item = target.classList.contains("accordion-tab")
+    ? target
+    : target.parentElement;
+  const group = item.dataset.actabGroup;
+  const id = item.dataset.actabId;
 
-	tabs.forEach(function(tab) {
-		if (tab.dataset.actabGroup === group) {
-			if (tab.dataset.actabId === id) {
-				tab.classList.add("accordion-active");
-			} else {
-				tab.classList.remove("accordion-active");
-			}
-		}
-	});
+  tabs.forEach(function (tab) {
+    if (tab.dataset.actabGroup === group) {
+      if (tab.dataset.actabId === id) {
+        tab.classList.add("accordion-active");
+      } else {
+        tab.classList.remove("accordion-active");
+      }
+    }
+  });
 
-	labels.forEach(function(label) {
-		const tabItem = label.parentElement;
+  labels.forEach(function (label) {
+    const tabItem = label.parentElement;
 
-		if (tabItem.dataset.actabGroup === group) {
-			if (tabItem.dataset.actabId === id) {
-				tabItem.classList.add("accordion-active");
-			} else {
-				tabItem.classList.remove("accordion-active");
-			}
-		}
-	});
+    if (tabItem.dataset.actabGroup === group) {
+      if (tabItem.dataset.actabId === id) {
+        tabItem.classList.add("accordion-active");
+      } else {
+        tabItem.classList.remove("accordion-active");
+      }
+    }
+  });
 }
 
-labels.forEach(function(label) {
-	label.addEventListener("click", toggleShow);
+labels.forEach(function (label) {
+  label.addEventListener("click", toggleShow);
 });
 
-tabs.forEach(function(tab) {
-	tab.addEventListener("click", toggleShow);
+tabs.forEach(function (tab) {
+  tab.addEventListener("click", toggleShow);
 });
 
+let openSelect = false;
+function openSelectCart() {
+  if (openSelect == false) {
+    document.querySelector(".select-list").classList.add("active");
+    openSelect = true;
+  } else {
+    document.querySelector(".select-list").classList.remove("active");
+    openSelect = false;
+  }
+}
+
+let district = false;
+function openDistrict() {
+  if (district == false) {
+    document.querySelector("#select-list").classList.add("active");
+    district= true;
+  } else {
+    document.querySelector("#select-list").classList.remove("active");
+    district = false;
+  }
+}
